e89bbc9bdfa1b799216c149e41d98465c1fc653a
AnikaBristy <bristy.technext@gmail.com>
2025-05-05 14:58:45 +0600

    Updates

---
+++ assets/Blank-C9PLK81G.js
@@ -0,0 +1,9 @@
+import{am as n,Z as l,_ as a,d as t,a1 as o,i as s,a6 as r,bA as d,ac as u}from"./index-Dfyd_qEf.js";import{a as i,e as m,V as c}from"./VCard-DG5wbeiI.js";const f={};function p(k,e){return u(),l(d,null,{default:a(()=>[t(c,{title:"This is blank page that use blank layout."},{default:a(()=>[t(i,{class:"bg-black"},{default:a(()=>e[0]||(e[0]=[o("pre",null,`{
+  path: '/blank',
+  name: 'blank',
+  component: () => import('@/views/Blank.vue'),
+  meta: {
+    layout: 'blank',
+  },
+},
+`,-1)])),_:1}),t(m,null,{default:a(()=>[t(s,{to:{name:"home"},color:"primary"},{default:a(()=>e[1]||(e[1]=[r(" Home ")])),_:1})]),_:1})]),_:1})]),_:1})}const b=n(f,[["render",p]]);export{b as default};
