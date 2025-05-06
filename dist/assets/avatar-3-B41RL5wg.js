79c01782563ad86d414b12e59c5e00e4ec47bb1a
mostofanobi <mntanveer9196@gmail.com>
2025-05-06 11:31:29 +0600

    added base & dist

---
+++ dist/assets/avatar-3-B41RL5wg.js
@@ -0,0 +1,1 @@
+const a="/velora/assets/avatar-1-B52fKW4C.png",s="/velora/assets/avatar-2-BWeDtiJR.png",t="/velora/assets/avatar-3-CVT-6Cz4.png";export{a,s as b,t as c};
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/velora/',
   plugins: [
     VueDevTools(),
     vue({
